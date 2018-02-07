951b32e52eecc3270ec3b6cd215d8c63a28b9bc8
Gerald Glocker <gerald.glocker@bosch-si.com>
2018-02-07 19:18:04 +0100

    initial commit of digital twin example

---
+++ digitaltwin-example/src/service/device-simulation.ts
@@ -0,0 +1,96 @@
+import * as fs from 'fs'
+import * as NodeWebSocket from 'ws'
+import * as HttpsProxyAgent from 'https-proxy-agent'
+import { ThingMessage, ThingMessageInfo } from '../util/thing-message'
+import { util } from '../util/util'
+import { setInterval } from 'timers'
+
+const CONFIG = JSON.parse(fs.readFileSync('config.json', 'utf8'))
+
+const WEBSOCKET_OPTIONS = {
+  agent: process.env.https_proxy ? new HttpsProxyAgent(process.env.https_proxy || process.env.HTTPS_PROXY) : null,
+  headers: {
+    ...CONFIG.websocketHeaders,
+    'Authorization': 'Basic ' + new Buffer(CONFIG.deviceSimulation.username + ':' + CONFIG.deviceSimulation.password).toString('base64')
+  }
+}
+const WEBSOCKET_REOPEN_TIMEOUT = 1000
+
+const THING_ID = CONFIG.deviceSimulation.thingId
+const THING_ID_PATH = THING_ID.replace(':', '/')
+
+export class DeviceSimulation {
+
+  private ws?: NodeWebSocket
+
+  private status: {
+    temperature?: number
+  } = {}
+
+  private config: {
+    threshold?: number
+  } = {}
+
+  // constructor() {
+  // }
+
+  start() {
+    console.log('[DeviceSimulation] start')
+
+    util.openWebSocket(CONFIG.websocketBaseUrl + '/ws/2', WEBSOCKET_OPTIONS, WEBSOCKET_REOPEN_TIMEOUT,
+      (ws) => {
+        this.ws = ws
+
+        this.ws.on('message', (data) => {
+          if (data.toString().startsWith('{')) {
+            this.process(new ThingMessage(JSON.parse(data.toString()) as ThingMessageInfo))
+          } else {
+            console.log('[DeviceSimulation] unprocessed non-json data: ' + data)
+          }
+        })
+
+        this.ws.send('START-SEND-EVENTS', (err) => console.log('[DeviceSimulation] ' + (err ? 'START-SEND-EVENTS websocket send error ' + err : 'START-SEND-EVENTS websocket send ok')))
+        this.ws.send('START-SEND-MESSAGES', (err) => console.log('[DeviceSimulation] ' + (err ? 'START-SEND-MESSAGES websocket send error ' + err : 'START-SEND-MESSAGES websocket send ok')))
+        this.ws.send('START-SEND-LIVE-COMMANDS', (err) => console.log('[DeviceSimulation] ' + (err ? 'START-SEND-LIVE-COMMANDS websocket send error ' + err : 'START-SEND-LIVE-COMMANDS websocket send ok')))
+        this.ws.send('START-SEND-LIVE-EVENTS', (err) => console.log('[DeviceSimulation] ' + (err ? 'START-SEND-LIVE-EVENTS websocket send error ' + err : 'START-SEND-LIVE-EVENTS websocket send ok')))
+
+        setInterval(() => this.updateStatus(), 4000)
+      })
+  }
+
+  private process(m: ThingMessage) {
+
+    if (m.channel === 'twin' && m.criterion === 'events' && m.action === 'modified'
+      && m.path.startsWith('/features/Device/properties/config')) {
+
+      if (THING_ID === m.thingId) {
+        const t = util.partial(m.path, m.value)
+        if (t.features.Device.properties.config.threshold) {
+          this.config.threshold = t.features.Device.properties.config.threshold
+          console.log('[DeviceSimulation] new threshold: ' + this.config.threshold)
+          return
+        }
+      }
+    }
+
+    console.log('[DeviceSimulation] unprocessed data: ' + m.topic + ' ' + m.thingId + ' ' + m.path + ' ' + m.status + ' ' + JSON.stringify(m.value))
+  }
+
+  private updateStatus() {
+    this.status.temperature = 18 + Math.random() * 10
+
+    if (!this.config.threshold || this.status.temperature > this.config.threshold) {
+      console.log(`[DeviceSimulation] send relevant temperature update: ${this.status.temperature}`)
+
+      const update: ThingMessage = new ThingMessage({
+        topic: THING_ID_PATH + '/things/twin/commands/modify',
+        path: '/features/Device/properties/status',
+        value: this.status,
+        headers: { 'response-required': false /*, 'correlation-id': '123'*/ }
+      })
+
+      this.ws!.send(JSON.stringify(update), (err) => { if (err) console.log('[DeviceSimulation] updateStatus websocket send error ' + err) })
+    }
+  }
+
+}

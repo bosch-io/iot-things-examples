ddb4d7085cae6a3f9608f715e1465d877c737e12
Gerald Glocker <gerald.glocker@bosch-si.com>
2018-02-19 19:04:19 +0100

    replace sleeps on startup by exact success conditions

--- digitaltwin-example/src/index.ts
+++ digitaltwin-example/src/index.ts
@@ -3,20 +3,15 @@ import { DeviceCommissioning } from './service/device-commissioning'
 import { DeviceSimulation } from './service/device-simulation'
 import { Accessories } from './service/accessories'
 
-async function sleep(milliseconds): Promise<void> {
-  return new Promise<void>(resolve => setTimeout(resolve, milliseconds))
-}
+// async function sleep(milliseconds): Promise<void> {
+//   return new Promise<void>(resolve => setTimeout(resolve, milliseconds))
+// }
 
 async function startAll() {
+  await new Accessories().start()
   await new DeviceCommissioning().start()
-
-  await sleep(5000)
   await new DeviceSimulation().start()
 
-  await sleep(5000)
-  await new Accessories().start()
-
-  await sleep(5000)
   await new Frontend().start()
 }
 

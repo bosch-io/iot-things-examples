cb3053dc593c03ce467946b8eac906061dbc790d
David Joos <david.joos@bosch-si.com>
2019-05-02 14:27:20 +0200

    Fixed indentation and code formatting.
    Signed-off-by: David Joos <david.joos@bosch-si.com>

--- octopus-bidirectional/iot-frontend/src/components/ThingList.vue
+++ octopus-bidirectional/iot-frontend/src/components/ThingList.vue
@@ -48,59 +48,64 @@
 </template>
 
 <script>
-export default {
-  name: "ThingList",
-  data() {
-    return {
-      cssIsActive:
-        "list-group-item list-group-item-action flex-column align-items-start active",
-      cssIsNotActive:
-        "list-group-item list-group-item-action flex-column align-items-start",
-      isActiveId: ""
-    };
-  },
-  methods: {
-    select(thing) {
-      if (thing === "newThing") thing = this.createNewThingTemplate();
-      this.$store.dispatch("handleSelected", thing).then(res => {
-        if (res.status == 200) {
-          this.isActiveId = thing.thingId;
+    export default {
+        name: "ThingList",
+        data() {
+            return {
+                cssIsActive:
+                    "list-group-item list-group-item-action flex-column align-items-start active",
+                cssIsNotActive:
+                    "list-group-item list-group-item-action flex-column align-items-start",
+                isActiveId: ""
+            };
+        },
+        methods: {
+            select(thing) {
+                if (thing === "newThing") {
+                    thing = this.createNewThingTemplate();
+                }
+                this.$store.dispatch("handleSelected", thing).then(res = > {
+                    if(res.status && res.status === 200
+            )
+                {
+                    this.isActiveId = thing.thingId;
+                }
+            })
+                ;
+            },
+            formatDate(item) {
+                /**
+                 * 0 : Year
+                 * 1 : Month
+                 * 2 : Day + Time
+                 * --------------
+                 * Second split
+                 * 0 : Day
+                 * 1 : Time + ?
+                 */
+                let yearMonth = item.split("-");
+                let day = yearMonth[2].split("T");
+                let time = day[1].split(".");
+                return `_modified: ${day[0]}.${yearMonth[1]}.${yearMonth[0]} - ${
+                    time[0]
+                    }`;
+            }
+        },
+        computed: {
+            items: {
+                get() {
+                    return this.$store.getters.getItems;
+                }
+            }
+        },
+        watch: {
+            items: function (val) {
+                if (this.isActiveId !== "") {
+                    this.isActiveId = "";
+                }
+            }
         }
-      });
-    },
-    formatDate(item) {
-      /**
-       * 0 : Year
-       * 1 : Month
-       * 2 : Day + Time
-       * --------------
-       * Second split
-       * 0 : Day
-       * 1 : Time + ?
-       */
-      let yearMonth = item.split("-");
-      let day = yearMonth[2].split("T");
-      let time = day[1].split(".");
-      return `_modified: ${day[0]}.${yearMonth[1]}.${yearMonth[0]} - ${
-        time[0]
-      }`;
-    }
-  },
-  computed: {
-    items: {
-      get() {
-        return this.$store.getters.getItems;
-      }
-    }
-  },
-  watch: {
-    items: function(val) {
-      if (this.isActiveId !== "") {
-        this.isActiveId = "";
-      }
-    }
-  }
-};
+    };
 </script>
 
 <style>
